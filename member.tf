5661e87e3d9eb6ec098a6dad0f5466c33657ab5a
adekunle ibitayo <adekunle.ibitayo@appvia.io>
2019-04-12 15:49:47 +0100

    Capitalisation in comment

--- member.tf
+++ member.tf
@@ -1,9 +1,9 @@
 # The invites send to the member accounts in each region
 resource "aws_guardduty_member" "eu_west_3" {
   count              = "${length(var.accounts)}"
-  account_id         = "${index(keys(var.accounts), count.index)}"
+  account_id         = "${element(keys(var.accounts), count.index)}"
   detector_id        = "${aws_guardduty_detector.eu_west_3.id}"
-  email              = "${lookup(var.accounts, index(keys(var.accounts), count.index))}"
+  email              = "${lookup(var.accounts, element(keys(var.accounts), count.index))}"
   invite             = true
   invitation_message = "${var.invite_message}"
   provider           = "aws.eu-west-3"
