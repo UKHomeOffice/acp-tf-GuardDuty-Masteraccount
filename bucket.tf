ad33a224b21a4fb3283eecf28724b4124f957abd
Chris Freeman <chris.freeman@digital.homeoffice.gov.uk>
2022-05-30 17:19:56 +0100

    kms policy

--- bucket.tf
+++ bucket.tf
@@ -72,6 +72,54 @@ data "aws_iam_policy_document" "kms_policy" {
       identifiers = ["guardduty.amazonaws.com"]
     }
   }
+  statement {
+    sid    = "IAMPermissions"
+    effect = "Allow"
+
+    resources = ["*"]
+
+    actions = [
+      "kms:*",
+    ]
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+      ]
+    }
+  }
+
+  statement {
+    sid    = "KeyAdministratorsPermissions"
+    effect = "Allow"
+
+    resources = ["*"]
+
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion",
+    ]
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+      ]
+    }
+  }
 }
 
 resource "aws_s3_bucket" "guardduty_bucket" {
@@ -90,9 +138,9 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 }
 
 resource "aws_kms_key" "guardduty_key" {
-  description             = "GuardDuty key for publishing events"
-  enable_key_rotation     = true
-  policy                  = data.aws_iam_policy_document.kms_policy.json
+  description         = "GuardDuty key for publishing events"
+  enable_key_rotation = true
+  policy              = data.aws_iam_policy_document.kms_policy.json
 }
 
 resource "aws_s3_bucket_versioning" "this" {
