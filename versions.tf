ceb7fd669d1f5d53fc92d59a69bcebc28a17f72a
Chris Freeman <chris.freeman@digital.homeoffice.gov.uk>
2022-06-06 15:41:11 +0100

    region versions.tf

--- versions.tf
+++ versions.tf
@@ -7,6 +7,7 @@ terraform {
       configuration_aliases = [
         aws.eu-west-2,
         aws.eu-west-1,
+        aws.eu-north-1,
         aws.us-east-1,
         aws.us-east-2,
         aws.us-west-2,
@@ -15,6 +16,7 @@ terraform {
         aws.ap-southeast-2,
         aws.ap-northeast-1,
         aws.ap-northeast-2,
+        aws.ap-northeast-3,
         aws.ap-south-1,
         aws.eu-central-1,
         aws.eu-west-3,
